b4df49be9e3f651f849f0497d325280866cae39a
Sotaneum <gnyontu39@gmail.com>
2024-02-03 14:04:03 +0900

    fix. jsx-attr-sort 버그 수정

--- tests/lib/rules/jsx-attr-sort.test.js
+++ tests/lib/rules/jsx-attr-sort.test.js
@@ -25,214 +25,151 @@ const ruleTester = new RuleTester({
   },
 });
 ruleTester.run("jsx-attr-sort", rule, {
-  valid: [`<Foo />`],
+  valid: [
+    `<Foo />`,
+    `<Foo check />`,
+    `<Foo title="Download" onClick={() => {}} />`,
+    `<Foo check title="Download" onClick={() => {}} />`,
+    `<Foo bold care kind love open />`,
+    `<Foo cry cake charm climb capture />`,
+    `<Foo onClick cry="" cake="" charm="" climb="" capture="" />`,
+    `<Foo cry={name} cake="" charm="" climb="" capture="" />`,
+    `<Foo charm="" climb="" capture="" {...props} cry={name} cake="" />`,
+    `<Foo
+      capture=""
 
+      charm=""
+      climb=""
+      {...props}
+      cry={name}
+      cake=""
+    />`,
+    `<Foo
+      // http props
+      capture=""
+
+      // ui props
+      cake
+      charm=""
+      climb=""
+      {...props}
+      cry={name}
+    />`,
+    `<Foo
+      // ui props
+      cake
+      charm=""
+      climb=""
+      // http props
+      capture=""
+      {...props}
+      cry={name}
+    />`,
+  ],
   invalid: [
     {
-      code: `<Foo aaaa={aaaa} bb={bb} onClick={handleClick} {...props} cacc={cccc} check/>`,
+      code: `<Foo onClick={() => {}} title="Download" />`,
       errors: 1,
-      output: `<Foo bb={bb} aaaa={aaaa} onClick={handleClick} {...props} check cacc={cccc}/>`,
+      output: `<Foo title="Download" onClick={() => {}} />`,
     },
     {
-      code: `<Foo /*test*/ aaaa={aaaa} bb={bb} onClick={handleClick} {...props} cacc={cccc} check/>`,
+      code: `<Foo onClick={() => {}} title="Download" check />`,
       errors: 1,
-      output: `<Foo bb={bb} /*test*/ aaaa={aaaa} onClick={handleClick} {...props} check cacc={cccc}/>`,
+      output: `<Foo check title="Download" onClick={() => {}} />`,
     },
     {
-      code: `<Foo
-        /*test*/
-        aaaa={aaaa}
-        bb={bb}
-        onClick={handleClick}
-        {...props}
-        cacc={cccc}
-        check
-      />`,
+      code: `<Foo love bold kind care open />`,
       errors: 1,
-      output: `<Foo
-        bb={bb}
-        /*test*/
-        aaaa={aaaa}
-        onClick={handleClick}
-        {...props}
-        check
-        cacc={cccc}
-      />`,
+      output: `<Foo bold care kind love open />`,
     },
     {
-      code: `<Foo
-        // test
-        aaaa={aaaa}
-        a={a}
-
-        // update
-        onClick={handleClick}
-        bb={bb}
-        {...props}
-        cacc={cccc}
-
-        // data
-        check
-        b={b}
-      />`,
+      code: `<Foo charm climb cry capture cake />`,
       errors: 1,
-      output: `<Foo
-        // test
-        a={a}
-        aaaa={aaaa}
-
-        // update
-        bb={bb}
-        onClick={handleClick}
-        {...props}
-        cacc={cccc}
-
-        // data
-        check
-        b={b}
-      />`,
+      output: `<Foo cry cake charm climb capture />`,
+    },
+    {
+      code: `<Foo cry="" climb="" capture="" cake="" charm="" onClick />`,
+      errors: 1,
+      output: `<Foo onClick cry="" cake="" charm="" climb="" capture="" />`,
+    },
+    {
+      code: `<Foo cake="" climb="" capture="" charm="" cry={name} />`,
+      errors: 1,
+      output: `<Foo cry={name} cake="" charm="" climb="" capture="" />`,
+    },
+    {
+      code: `<Foo capture="" charm="" climb="" {...props} cake="" cry={name} />`,
+      errors: 1,
+      output: `<Foo charm="" climb="" capture="" {...props} cry={name} cake="" />`,
     },
     {
       code: `<Foo
-        aaaa={aaaa}
-        a={a}
+      capture=""
 
-        onClick={handleClick}
-        bb={bb}
-        {...props}
-        cacc={cccc}
+      climb=""
+      charm=""
+      {...props}
+      cake=""
+      cry={name}
+    />`,
+      errors: 1,
+      output: `<Foo
+      capture=""
 
-        check
-        b={b}
-      />`,
-      errors: 1,
-      output: `<Foo
-        a={a}
-        aaaa={aaaa}
+      charm=""
+      climb=""
+      {...props}
+      cry={name}
+      cake=""
+    />`,
+    },
+    {
+      code: `<Foo
+      // http props
+      capture=""
 
-        bb={bb}
-        onClick={handleClick}
-        {...props}
-        cacc={cccc}
+      // ui props
+      charm=""
+      climb=""
+      cake
+      {...props}
+      cry={name}
+    />`,
+      errors: 1,
+      output: `<Foo
+      // http props
+      capture=""
 
-        check
-        b={b}
-      />`,
+      // ui props
+      cake
+      charm=""
+      climb=""
+      {...props}
+      cry={name}
+    />`,
     },
     {
       code: `<Foo
-        aaaa={aaaa}
-        a={a}
-        onClick={handleClick}
-        bb={bb}
-        {...props}
-        cacc={cccc}
-        check
-        b={b}
-      />`,
+      // ui props
+      climb=""
+      cake
+      charm=""
+      // http props
+      capture=""
+      {...props}
+      cry={name}
+    />`,
       errors: 1,
       output: `<Foo
-        a={a}
-        bb={bb}
-        aaaa={aaaa}
-        onClick={handleClick}
-        {...props}
-        check
-        b={b}
-        cacc={cccc}
-      />`,
-    },
-    {
-      code: `<Foo
-        aaaa={aaaa}
-        a={a}
-        onClick={handleClick}
-        bb={bb}
-        cacc={cccc}
-        check
-        b={b}
-      />`,
-      errors: 1,
-      output: `<Foo
-        check
-        a={a}
-        b={b}
-        bb={bb}
-        aaaa={aaaa}
-        cacc={cccc}
-        onClick={handleClick}
-      />`,
-    },
-    {
-      code: `<Foo
-        aaaa={{b:1, a:1}}
-        a={a}
-        onClick={handleClick}
-        bb={bb}
-        cacc={cccc}
-        check
-        b={b}
-      />`,
-      errors: 1,
-      output: `<Foo
-        check
-        a={a}
-        b={b}
-        bb={bb}
-        aaaa={{b:1, a:1}}
-        cacc={cccc}
-        onClick={handleClick}
-      />`,
-    },
-    {
-      code: `<Foo
-        aaaa={{b:1, a:1}}
-        a={a}
-        onClick={handleClick}
-        bb={bb}
-        cacc={cccc}
-        check
-        b={b}
-      ><p b={a} a={b}>test</p></Foo>`,
-      errors: 1,
-      output: `<Foo
-        check
-        a={a}
-        b={b}
-        bb={bb}
-        aaaa={{b:1, a:1}}
-        cacc={cccc}
-        onClick={handleClick}
-      ><p a={b} b={a}>test</p></Foo>`,
-    },
-    {
-      code: `<Foo
-        aaaa={{b:1, a:1}}
-        a={a}
-        onClick={handleClick}
-        bb={bb}
-        cacc={cccc}
-        check
-        b={b}
-      >
-        <p b={a} a={b}>
-          test
-        </p>
-      </Foo>`,
-      errors: 1,
-      output: `<Foo
-        check
-        a={a}
-        b={b}
-        bb={bb}
-        aaaa={{b:1, a:1}}
-        cacc={cccc}
-        onClick={handleClick}
-      >
-        <p a={b} b={a}>
-          test
-        </p>
-      </Foo>`,
+      // ui props
+      cake
+      charm=""
+      climb=""
+      // http props
+      capture=""
+      {...props}
+      cry={name}
+    />`,
     },
   ],
 });
